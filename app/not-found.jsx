50e311f345faa066577e3cece98b4d369290206f
Milton <ajaxmilton@hotmail.com>
2026-03-17 15:24:53 +0200

    Refine styles in 404 page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,13 +3,16 @@ import Link from "next/link";
 export default function notfound() {
   return (
     <div className="max-w-6xl mx-auto h-[calc(100vh-72px)] flex items-center justify-center">
-      <div className="flex items-center justify-center flex-col space-y-2 text-shadow-sm">
+      <div className="flex items-center justify-center flex-col space-y-3 text-shadow-sm">
         <h2 className="text-8xl text-[#0db0eb] font-bold ">404</h2>
-        <h3 className="text-2xl text-[#0b344e]">Development in progress</h3>
-        <p className="text-gray-500">Please try again later</p>
+        <div className="flex flex-col items-center justify-center">
+          <h3 className="text-2xl text-[#0b344e]">Development in progress</h3>
+          <p className="text-gray-500">Please try again later</p>
+        </div>
+
         <Link
           href={"/"}
-          className="py-2 px-10 bg-[#0db0eb]  text-white font-semibold "
+          className="py-2 px-8 bg-[#0db0eb] mt-2 text-white font-semibold "
         >
           Visit Homepage
         </Link>
